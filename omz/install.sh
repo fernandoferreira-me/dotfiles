e1a38512a4374f0038b1c977753c2fa662200a6a
Fernando Ferreira <fernandogferreira@gmail.com>
2023-01-25 13:50:24 -0300

    Zsh set

---
+++ omz/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+set -e
+
+if [ -d "${HOME}/.oh-my-zsh" ]; then
+  printf "oh-my-zsh is already installed\n"
+else
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ln -sf "${DOTFILES_LOCATION}/omz/.p10k.zsh" "${HOME}/.p10k.zsh"
